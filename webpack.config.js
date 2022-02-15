d29b04780086df9ef83a8686c5994dce24e1accb
Raluca Ciocoiu <raluca.ciocoiu2@klarna.com>
2022-02-15 14:21:17 +0200

    Webpack Linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+// const eslint = new ESLintPlugin();
 
 module.exports = (env, argv) => {
   console.log(`This is the Webpack 4 'mode': ${argv.mode}`);
@@ -7,6 +9,11 @@ module.exports = (env, argv) => {
       devtool: 'eval-source-map',
       entry: './src/index.ts',
       mode: 'development',
+      options: {
+          //eslint options
+          cache: true,
+          fix: true,
+      }
       module: {
         rules: [{
             exclude: /node_modules/,
@@ -15,9 +22,11 @@ module.exports = (env, argv) => {
               extensions: ['.ts', '.tsx'],
             },
             test: /\.ts|tsx$/,
-            use: 'ts-loader',
+            use: ['ts-loader', 'eslint-loader'],
         }],
       },
-      plugins: [new HtmlWebpackPlugin()]
+      plugins:
+          [new HtmlWebpackPlugin(), new ESLintPlugin(options)]
+
   })
 }
